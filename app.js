d3588b5c3abfef68152d6c3065e6e9b1eee810cf
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-04-30 12:48:48 +0200

    App ready for demo

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-// default value for title local
+// default value for title !!!!!!!!
 app.locals.title = 'Park Finder | Find your park!';
 
 // Routes middleware goes here7
